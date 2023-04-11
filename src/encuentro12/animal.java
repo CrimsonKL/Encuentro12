d0d3c3f095d327e89c36f8056e7b15d4b697980f
Juan Maria Salinas <juanmsalinas08@gmail.com>
2023-04-10 21:49:08 -0300

    otro commit mas

---
+++ src/encuentro12/animal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encuentro12;
+
+/**
+ *
+ * @author Salinas
+ */
+public class animal {
+    
+}
